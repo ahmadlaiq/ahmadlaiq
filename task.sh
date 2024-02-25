46d0898babc789c1d7af4969f4a1c3b4f0fa1765
Ahmad Nurul Laiq <ahmadlaiq97@gmail.com>
2024-02-25 22:37:46 +0700

    Create task.sh

---
+++ task.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+arr[0]="bot: 👋"
+arr[1]="bot: 🥳"
+arr[2]="bot: 😬"
+arr[3]="bot: 👨‍💻"
+arr[4]="bot: 😪"
+arr[5]="bot: 😎"
+arr[6]="bot: 🙄"
+arr[7]="bot: 👻"
+
+rand=$[$RANDOM % ${#arr[@]}]
+d=`date '+%Y-%m-%dT%H:%M:%SZ'`
+
+echo "## 🤔 LAST UPDATED AT: ${d}" > README.md
+
+git config --local user.email "ahmadlaiq97@gmail.com"
+git config --local user.name "Ahmad Nurul Laiq"
+git commit -am "${arr[$rand]} (at ${d})"
